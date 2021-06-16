911e8eb5aa8c33de4053e058f8c3bd77161e874e
Ahmad Abdulaziz Sorour <47137234+zSorour@users.noreply.github.com>
2021-06-16 17:34:18 +0200

    added comments and documentation

--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,101 @@
+/*React and Next.JS related imports*/
 import { useEffect, useState } from 'react';
 import Router from 'next/router';
-import { v4 as uuid } from 'uuid';
-import axios from 'axios';
+
+import { v4 as uuid } from 'uuid'; //For generating universal unique IDs.
+import axios from 'axios'; //For easier HTTP requests handling.
+
+//Import the Tau-Prolog session setup in prolog.js file.
 import plSession from '../prolog/prolog';
 
-
+//Import CSS styles for the webpage.
 import classes from '../styles/HomePage.module.css';
 
-const HomePage = () => {
-  const [questionsAnswersState, setQA] = useState([]);
-  const [questionsCount, setQuestionsCount] = useState(0);
-  const [accessToken, setAccessToken] = useState('');
 
+//Create a React Functional Component that renders JSX (HTML in JavaScript).
+const HomePage = () => {
+  //Setting up some state variables to be used for rendering later
+  const [questionsAnswersState, setQA] = useState([]); //Array of objects to hold questions and answers from Prolog Agent contacting the server.
+  const [questionsCount, setQuestionsCount] = useState(0); //Integer counter to hold the number of questions asked.
+  const [accessToken, setAccessToken] = useState(''); //String variable to hold the authorization JWT retrieved from the server. 
+  const [error, setError] = useState();
+
+  //useEffect hook, with the following definition, is a React special function 'hook' that is executed once this component is created and rendered.
   useEffect(async () => {
-    //Connect to server to get token, similar to starting a session:
+    //On first time rendering the component, connect to the server, get a JWT and store it in the accessToken state variable.
     axios.post('http://localhost:8080/auth', {
       connectionID: uuid()
     })
       .then(async (response) => {
+        //After the request is sent and a response is received, the response should be containing the access token, JWT.
+        //We call the setAccessToken function to store it in the accessToken state variable.
         setAccessToken(response.data.accessToken);
+
+        //Having loaded the token, we use TauProlog plSession imported from prolog.js file to consult the knowledge base. 
         await plSession.promiseConsult('kb.pl');
+        //After the KB is consulted successfully, we query 'start.' to start the guessing process.
         await plSession.promiseQuery('start.');
         await plSession.promiseAnswer();
+      })
+      .catch(err => {
+        setError('Error connecting to server. Please make sure that the server is up and running');
       });
   }, []);
 
-  //Define global functions so it can be called from the kb.pl file using tau-prolog
+  /*DEFINE SOME HELPER FUNCTIONS IN THE GLOBAL VARIABLE SO THAT IT CAN BE CALLED FROM TAUPROLOG*/
+  //A funcion that returns the access token from accessToken state variable.
   global.getToken = function () {
     return accessToken;
   }
-
+  //A function that calls setQuestionsCount and setQA methods that update the state variables of the questions and answers, and the questions count
   global.setQuestionAnswer = function (question, answer) {
-    question = question.split('_').join(' ');
+    //Since we write text-data in prolog with underscores instead of spaces between the words,
+    //we replace all the underscores with whitespace.
+    question = question.replace(/_/g, ' ');
     setQuestionsCount(questionsCount => questionsCount + 1);
     setQA(questionsAnswersState => [...questionsAnswersState, { question: question, answer: answer }]);
   }
 
-  return (
-    <div>
-      <div className={classes.Conversation}>
-        <h1>Interaction between Client and Server</h1>
-        <h2>Number of questions asked: {questionsCount}</h2>
-        <div className={classes.Container}>
-          <div className={classes.Conversation_Body}>
-            {questionsAnswersState.map((item, index) => (
-              <div key={item.question + item.answer} className={classes.Message}>
-                <p className={classes.Question}>
-                  Client: {questionsAnswersState[index + 1] ? `does the character have the following feature: "${item.question}"?` : `Is the character "${item.question}"?`}
-                </p>
-                <p className={classes.Answer}>
-                  Server: {item.answer}
-                </p>
-              </div>
-            ))}
+  /*
+  The React functional component returns JSX elements that represent HTML elements to be rendered, where JavaScript can be used for added dynamicality
+  */
+
+  if (error) {
+    return (<p>{error}</p>);
+  }
+  else {
+    return (
+      <div>
+        <div className={classes.Conversation}>
+          <h1>Interaction between Client and Server</h1>
+          <h2>Number of questions asked: {questionsCount}</h2>
+          <div className={classes.Container}>
+            <div className={classes.Conversation_Body}>
+              {questionsAnswersState.map((item, index) => (
+                <div key={item.question + item.answer} className={classes.Message}>
+                  <p className={classes.Question}>
+                    Client: {questionsAnswersState[index + 1] ? `does the character have the following feature: "${item.question}"?` : `Is the character "${item.question}"?`}
+                  </p>
+                  <p className={classes.Answer}>
+                    Server: {item.answer}
+                  </p>
+                </div>
+              ))}
+            </div>
           </div>
+          <button className={classes.AgainButton} onClick={() => Router.reload()}>
+            Guess Again
+          </button>
         </div>
-        <button className={classes.AgainButton} onClick={() => Router.reload()}>
-          Guess Again
-        </button>
+        <div className={classes.SoftwareTools}>
+          <a href="https://reactjs.org/"><img src="/reactLogo.png" width={70} height={70} /></a>
+          <a href="https://nextjs.org/"><img src="/nextjsLogo.png" width={70} height={70} /></a>
+          <a href="http://tau-prolog.org/"><img src="/tauprologLogo.png" width={70} height={70} /></a>
+          <a href="https://nodejs.org/en/"><img src="/nodejsLogo.png" width={70} height={70} /></a>
+        </div>
       </div>
-      <div className={classes.SoftwareTools}>
-        <a href="https://reactjs.org/"><img src="/reactLogo.png" width={70} height={70} /></a>
-        <a href="https://nextjs.org/"><img src="/nextjsLogo.png" width={70} height={70} /></a>
-        <a href="http://tau-prolog.org/"><img src="/tauprologLogo.png" width={70} height={70} /></a>
-        <a href="https://nodejs.org/en/"><img src="/nodejsLogo.png" width={70} height={70} /></a>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default HomePage;
--- public/kb.pl
+++ public/kb.pl
@@ -1,9 +1,15 @@
 /*Import JS Tau-Prolog library to invoke JS functions later*/
 :- use_module(library(js)).
 
+/*Starting clause to be queried once the Prolog Knowledge Base is loaded*/
 start :- guess(Person), ask(Person), undo.
 
 /* guesses to be tested */
+/*Once the start clause is queried, the guess(Person) clause gets queried and Prolog
+internally creates a search tree structure that is traversed in a DFS fashion until it unifies the Person variable
+with the match character*/
+
+/*The cut operator '!' prevents backtracking after finding a match*/
 guess(mohamed_salah)   :- mohamed_salah, !.
 guess(halle_berry)     :- halle_berry, !.
 guess(messi) :- messi, !.
@@ -16,7 +22,7 @@ guess(leonardo_dicaprio) :- leonardo_dicaprio, !.
 guess(ed_sheeran) :- ed_sheeran, !.
 guess(will_smith) :- will_smith, !.
 guess(ronaldo) :- ronaldo, !.
-guess(unknown).
+guess(unknown_character).
 
 /* Person identification rules */
 mohamed_salah :- verify(male),
@@ -79,7 +85,7 @@ ronaldo :- verify(male),
            verify(juventus_player),
            verify(portuguese).           
 
-/* Facts about some features */
+/* Categorizing related facts in terms of profession and nationality*/
 profession(footballer).
 profession(actor).
 profession(singer).
@@ -92,26 +98,41 @@ nationality(portuguese).
 nationality(argentine).
 
 
-/* classification rules */
+/* Rules */
 female:- \+ verify(male).
 no(X):- yes(Y), profession(X), profession(Y).
 no(X):- yes(Y), nationality(X), nationality(Y).
 
 
-/* Verify feature */
+/* Verify a Feature by checking whether we have a yes(Feature) or no(Feature)
+  If we have yes(Feature), this means that we already know that the feature exists and no need to ask the server,
+  if we have no(Feature), this means that we alreadu know that the feature does not exist and there is no need to ask the server,
+  else, we have neither yes or no answer and we do not know, hence we ask the server using ask(Feature).
+*/
 verify(Feature) :-
    (yes(Feature) -> true ;
     (no(Feature) -> fail ;
     ask(Feature))).
 
-
+/*
+Define a clause setQA/2 that takes Question and Answer variables and invokes a JavaScript function
+passing to it the Question and Answer variables.
+Function invokation is done using Tau-Prolog's built-in predicates prop/2 that gets an object from the Global JS Object,
+and the predicate apply/3 which invokes the function represented in the object retrieved from prop/2.
+The last argument of apply/3 represents the return value of the function. Since the function does not return anything,
+and we are not concerned with the returned value anyway, we set an anonymous varaible using _.
+*/
 setQA(Question, Answer) :- prop(setQuestionAnswer, SetQuestionAnswer), apply(SetQuestionAnswer, [Question, Answer], _).
-getToken :- prop(getToken, GetToken), apply(GetToken, [_], Token), Token.
-/* How to ask questions */
 
 /*
-Ask the server whether this feature exists in the target character or not,
-Read the response and assert accordingly
+Ask the server whether a feature exists in the target character or not,
+Read the response and assert accordingly.
+
+We use atom_concat/3 to form the request url string.
+atom_concat/3 is also used to form the authorization header value by concatenating BEARER with the token itself,
+following the standard of JWT authoriaztion.
+
+We make HTTP Request to the server using Tau-Prolog's predicate ajax/4.
 */
 ask(Question) :-
     atom_concat('http://localhost:8080/ask?q=', Question, URL),
@@ -124,10 +145,18 @@ ask(Question) :-
        assertz(yes(Question)) ;
        assertz(no(Question)), fail).
 
+/*
+Inform the interpreter that the defintion of predicates yes and no may change throughout running the program,
+for example asserting or retracting facts regarding the predicate.
+*/
 :- dynamic(yes/1).
 :- dynamic(no/1).
 
-/* undo all yes/no assertions */
+
+/*
+Define undo predicate to retract (delete) all dynamic assertions created previously for the yes and no predicates.
+This typically happens after the character is correctly guessed.
+*/
 undo :- retract(yes(_)),fail.
 undo :- retract(no(_)),fail.
 undo.
